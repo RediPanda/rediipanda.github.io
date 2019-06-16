1566156f036cfd7dbb77065dc07901d928cea190
RediPanda <jadentruong2002@gmail.com>
2019-06-16 16:55:26 +1000

    World update

---
+++ CustomWorldLoader Maps/world.bat
@@ -0,0 +1,49 @@
+@ECHO OFF
+TITLE Module A Introduction // %applicationName%
+CLS
+
+:GREETINGS
+echo.
+echo.
+echo Hello %username%,
+echo.
+echo Welcome to my Custom World for my Module A English Assignment of 19/06/19.
+echo.
+echo.
+echo Please, enjoy this multimodal story as I have worked on this atleast more than 3 hours a day, including the
+echo main game engine this world is running off.
+echo.
+echo.
+echo Interesting Facts about TextGame while we're loading the required data for this world.
+echo.
+echo Project Name: TextGame
+echo.
+echo 	Project Coding Language: Batch Coding (Windows Console Command Prompt).
+echo.
+echo.
+echo 	Project was officially started on the 29th May, 2017.
+echo 	The initial idea was to create a base game using the old and classical black/white approach.
+echo.
+echo 	Between the period of May 2018 -^> July 2018, the Project work was halted due to lack of motivation.
+echo.
+echo 	Eventually, it kickstarted back in September 2018 and receives support weekly.
+echo.
+echo.
+echo	About the Project:
+echo.
+echo	The project file (as of 16/06/19) consists of:
+echo		Game Engine:		Lines:			Length (Character Count):
+echo		CWL Mod:			Lines:			Length (Character Count):
+echo		This World:			Lines:			Length (Character Count):
+echo.
+echo 	All project files are hosted using GitHub, a repository site for developers.
+echo.
+echo 	All project files are edited using the tool: Notepad++.
+echo.
+echo. 	Batch Coding is very easy. As long as you got the motivation and the dedication to learn a new language, it's easy to pick up.
+echo.
+echo.
+echo 	** IF YOU DO WISH TO REPLAY THIS WORLD TO PLAY A DIFFERENT PATHWAY, OPEN THE README FILE AND HEAD TO SECTION: #Resetting CWL Data
+echo.
+echo.
+echo Thank you for taking your time for browsing through this multimodal assignment. Have a good day and enjoy!
